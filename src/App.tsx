2d970b97cd721bf17bf82b6b1d5890def3f41c80
DanPin <dany.pineault@videotron.ca>
2022-04-05 06:28:46 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { render } from "react-dom";
+// import "./index.css";
+// import { ThemeProvider } from "@mui/material";
+// import CssBaseline from "@mui/material/CssBaseline";
+// import Icon from "@mui/material/Icon";
+// import { Provider } from "react-redux";
+// import Layout from "./layout/Layout";
+// import store from "./store";
+// import theme from "./asset/theme/theme";
+// import themeDark from "./asset/theme-dark/theme";
+
+const App = () => <h3>Welcome from App</h3>;
+
+render(<App />, document.getElementById("root"));
